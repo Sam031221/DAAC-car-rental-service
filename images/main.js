67c54de057902bd85771c5c7f38938944c1fd421
sam031221 <samhouseofbrides@gmail.com>
2023-12-02 15:00:08 +0100

    Initial commit

---
+++ images/main.js
@@ -0,0 +1,11 @@
+
+const navigation = document.querySelector('.navigation')
+const cancel = document.querySelector('.bi-x')
+const list = document.querySelector('.bi-list')
+const ham = document.querySelector('.hamburger')
+
+
+function show(){
+ navigation.classList.toggle('open')
+ ham.classList.toggle('open')
+}
